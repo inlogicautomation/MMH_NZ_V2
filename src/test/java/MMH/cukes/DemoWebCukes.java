65059339cf221927fce2645a9bd36e01e6e8d787
bbhaanureaka@inlogic.in <bbhaanureaka@inlogic.in>
2022-06-21 19:55:18 +0530

    Edge driver update.
    By,
    Patrick

--- src/test/java/MMH/cukes/DemoWebCukes.java
+++ src/test/java/MMH/cukes/DemoWebCukes.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/resources/features/Regression"},
         monochrome = true,
-        tags = "@Login",
+        tags = "@APPOINTMENTS",
         glue = {"MMH"},
         plugin = {
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
