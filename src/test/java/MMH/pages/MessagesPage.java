1a32afaeafabd0b791c9b99ee17069ec1b97f300
Patrick <v-patrick@mmhglobal.com>
2022-10-18 09:14:53 +0530

    happy path xpath changes for the messages module 18/10/22

--- src/test/java/MMH/pages/MessagesPage.java
+++ src/test/java/MMH/pages/MessagesPage.java
@@ -338,7 +338,7 @@ public class MessagesPage extends BasePage {
             .toString();
 
     protected String messageText = new StringBuilder()
-            .append("//div/p[contains(text(),'")
+            .append("//p[contains(text(),'")
             .append("<<REPLACEMENT>>")
             .append("')]")
             .toString();
@@ -500,9 +500,13 @@ public class MessagesPage extends BasePage {
             .append(Constants.IMAGES_FOLDER).append(File.separator)
             .append("<<FILENAME>>").toString();
 
-    @FindBy(how = How.XPATH, using = "(//mat-icon[contains(text(),'attachment')])[2]")
+    @FindBy(how = How.XPATH, using = "//input[@formcontrolname='Attachment']")
     protected WebElement btnFloorplanUpload;
 
+    @FindBy(how = How.XPATH, using = "//input[@type='file']")
+    protected WebElement btnDoctorUploadfile;
+
+    //input[@type='file']
     @FindBy(how = How.XPATH, using = "//input[@value='Attach']")
     protected WebElement btndevUpload;
 
@@ -782,11 +786,11 @@ public class MessagesPage extends BasePage {
             waitForSeconds(2);
             waitForElement(elmntMessages);
             waitForElementClickable(elmntMessages);
-            click(elmntMessages);
+            jsClick(elmntMessages);
             waitForSeconds(1);
             waitForElement(elmntSentPatient);
             waitForElementClickable(elmntSentPatient);
-            click(elmntSentPatient);
+            jsClick(elmntSentPatient);
             waitForElementDisappear(driver, By.xpath(elmntSpinner));
             waitForSeconds(1);
             String PageUrl = driver.getCurrentUrl();
@@ -3218,7 +3222,7 @@ public class MessagesPage extends BasePage {
             System.out.println("Document Name" + strUploadDocumentName);
             String strFloorplanDocumentName = strFloorplanFilePath.replace("<<FILENAME>>", strUploadDocumentName);
             System.out.println(strFloorplanDocumentName);
-            btnFloorplanUpload.sendKeys(strFloorplanDocumentName);
+            btnDoctorUploadfile.sendKeys(strFloorplanDocumentName);
             waitForSeconds(5);
             waitForElementClickable(btndevUpload);
             jsClick(btndevUpload);
